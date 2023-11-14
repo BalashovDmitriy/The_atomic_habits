9c5b8360b3b87adfec2f63f1793ef931727bdd9a
dmitriy.balashov <reaver_std@mail.ru>
2023-11-14 12:49:46 +0500

    add Habit CRUD

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from habit.models import Habit
+from habit.permissions import IsOwner
+from habit.serializers import HabitSerializer, HabitListSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
